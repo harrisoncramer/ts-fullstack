cd9e909ef52d0840ce38d3cc217aac115108fc50
Harrison Cramer <harrisoncramer@gmail.com>
2024-08-03 09:45:08 -0400

    Added initial setup (tailwind, vite, vue, ts)

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import SomeComponent from './components/SomeComponent.vue'
+</script>
+
+<template>
+  <SomeComponent message="Your template is running!" />
+</template>
---
+++ src/components/SomeComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>{{ message }}</p>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+type Props = {
+  message?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  message: 'Some default message',
+})
+</script>
+<style lang="postcss" scoped>
+p {
+  @apply text-red-200;
+}
+</style>
