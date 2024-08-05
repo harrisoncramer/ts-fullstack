ab4f1a66a3b2d69327809d645e617e93c9887938
Harrison Cramer <harrisoncramer@gmail.com>
2024-08-05 16:02:14 -0400

    Added debug middleware

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
+import debug from "./middleware/debug";
 
 const app = express()
 app.use(express.json())
+if(process.env.DEBUG) app.use(debug)
 
 app.get('/api/v1/users', (_req: Request, res: Response) => {
   res.status(200).send([
