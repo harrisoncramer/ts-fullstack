c729b34e341ff4260758c526fea1e2aaa3259186
Harrison Cramer <harrisoncramer@gmail.com>
2024-08-07 09:41:24 -0400

    Updated server/app.ts

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,9 @@
 import express from "express"
 import debug from "@/middleware/debug"
-import db from "@/db"
 import usersRouter from "@/routers/users"
 
 const app = express()
 app.use(express.json())
-app.set("db", db)
 if(process.env.DEBUG) app.use(debug)
 
 /* Routers */
