2833276438f5730e3ad8713a4c6b6604c78283b7
Harrison Cramer <harrisoncramer@gmail.com>
2024-08-07 12:59:38 -0400

    Updated src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import './style.css'
+import '@/style.css'
 import 'normalize.css'
 import { createRouter } from "./router"
 import App from './App.vue'
