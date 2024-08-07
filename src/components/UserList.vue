b8f5ac403d245b81a78be2696b970c85cd4ae5db
Harrison Cramer <harrisoncramer@gmail.com>
2024-08-07 11:45:24 -0400

    Moved user form

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -6,12 +6,10 @@
   >
     {{ user.first_name }} {{ user.last_name }} (Id is {{ user.id }})
   </div>
-  <NewUserForm />
 </template>
 
 <script setup lang="ts">
 import { User } from '@/stores/users'
-import NewUserForm from './NewUserForm.vue'
 
 type Props = {
   users: User[]
